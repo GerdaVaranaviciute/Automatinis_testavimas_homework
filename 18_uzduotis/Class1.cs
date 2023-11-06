fae97429481c04bc7d8efd3450b3127d61d101e5
GerdaVaranaviciute <gerda.va@LT235-PC663>
2023-11-06 16:11:30 +0200

    Add project files.

---
+++ 18_uzduotis/Class1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace _18_uzduotis
+{
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://demoqa.com/text-box";
+
+            IWebElement inputName = driver.FindElement(By.XPath("//*[@id=\"userName\"]"));
+            inputName.SendKeys("Gerda");
+
+            IWebElement inputEmail = driver.FindElement(By.XPath("//*[@id=\"userEmail\"]"));
+            inputEmail.SendKeys("varanaviciute.gerda@gmail.com");
+
+            IWebElement inputCurrentAddress = driver.FindElement(By.XPath("//*[@id=\"currentAddress\"]"));
+            inputCurrentAddress.SendKeys("Vilnius");
+
+            IWebElement inputPernamentAddress = driver.FindElement(By.XPath("//*[@id=\"permanentAddress\"]"));
+            inputPernamentAddress.SendKeys("Zarasai");
+
+            IWebElement buttonSubmit = driver.FindElement(By.XPath("//*[@id=\"submit\"]"));
+            buttonSubmit.Click();
+
+
+        }
+
+    }
+}
